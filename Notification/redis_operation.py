36692e6071156f4470adb25fbc8a6949d2e7adcf
Woody Huang <woody1573@163.com>
2016-04-06 10:31:20 +0800

    修复了redis操作的问题

--- Notification/redis_operation.py
+++ Notification/redis_operation.py
@@ -6,25 +6,31 @@ r = redis.Redis(db=REDIS_DB_INDEX, host='localhost', port=6379)
 
 def incr_notif(key, badge):
     notif_num = r.incrby("notif:{}".format(key), badge)
-    chat_num = int(r.get("chat:{}".format(key)))
+    chat_num = r.get("chat:{}".format(key))
     if chat_num is None:
         chat_num = 0
+    else:
+        chat_num = int(chat_num)
     return notif_num + chat_num
 
 
 def incr_chat(key, badge):
     chat_num = r.incrby("chat:{}".format(key), badge)
-    notif_num = int(r.get("notif:{}".format(key)))
+    notif_num = r.get("notif:{}".format(key))
     if notif_num is None:
         notif_num = 0
+    else:
+        notif_num = int(notif_num)
     return chat_num + notif_num
 
 
 def clear_notif(key):
     r.delete("notif:{}".format(key))
-    chat_num = int(r.get("chat:{}".format(key)))
+    chat_num = r.get("chat:{}".format(key))
     if chat_num is None:
         chat_num = 0
+    else:
+        chat_num = int(chat_num)
     return chat_num
 
 
