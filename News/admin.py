17422f7930ca49ccab449bc3ef01295e92fd268c
Woody Huang <woody1573@163.com>
2016-07-18 14:12:38 +0800

    done

--- News/admin.py
+++ News/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import register
+from .models import News
+
+
+@register(News)
+class NewsAdmin(admin.ModelAdmin):
+    exclude = ("liked_by", "shared_times", "created_at", "video")
+    search_fields = ('title', )
