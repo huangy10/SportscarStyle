ae6c0f87be0228f3511bff664e5c286eac2fd44f
Woody Huang <woody1573@163.com>
2016-08-15 22:46:30 +0800

    fix errors in billboard updating

--- Club/tasks.py
+++ Club/tasks.py
@@ -27,37 +27,39 @@ def update_billboard():
     else:
         next_billboard_version = latest_billboard_obj.version + 1
 
+    def update_club_at_billboard(filter_type, new_list, scope):
+        for order, club in enumerate(new_list, 1):
+            try:
+                old_billboard = ClubBillboard.objects.get(
+                    club=club, version=next_billboard_version - 1, scope=scope, filter_type=filter_type
+                )
+                old_order = old_billboard.order
+                ClubBillboard.objects.create(
+                    club=club, order=order, d_order=order - old_order, scope=scope, filter_type=filter_type,
+                    new_to_list=False, version=next_billboard_version
+                )
+            except ObjectDoesNotExist:
+                ClubBillboard.objects.create(
+                    club=club, order=order, d_order=0, scope=scope, filter_type=filter_type, new_to_list=True,
+                    version=next_billboard_version
+                )
+
     for (_, prov) in cities.items():
         for city in prov[0].keys():
-
-            def update_club_at_billboard(filter_type, new_list):
-                for order, club in enumerate(new_list, 1):
-                    try:
-                        old_billboard = ClubBillboard.objects.get(
-                            club=club, version=next_billboard_version - 1, scope=city, filter_type=filter_type
-                        )
-                        old_order = old_billboard.order
-                        ClubBillboard.objects.create(
-                            club=club, order=order, d_order=order - old_order, scope=city, filter_type=filter_type,
-                            new_to_list=False, version=next_billboard_version
-                        )
-                    except ObjectDoesNotExist:
-                        ClubBillboard.objects.create(
-                            club=club, order=order, d_order=0, scope=city, filter_type=filter_type, new_to_list=True,
-                            version=next_billboard_version
-                        )
-
             update_club_at_billboard(
                 "total",
-                Club.objects.filter(identified=True).order_by("-value_total")[0:MAX_ORDER]
+                Club.objects.filter(identified=True).order_by("-value_total")[0:MAX_ORDER],
+                city
             )
             update_club_at_billboard(
                 'average',
-                Club.objects.filter(identified=True).order_by('-value_average')[0:MAX_ORDER]
+                Club.objects.filter(identified=True).order_by('-value_average')[0:MAX_ORDER],
+                city
             )
             update_club_at_billboard(
                 'members',
-                Club.objects.annotate(members_num=Count("members")).order_by('-members_num')[0:MAX_ORDER]
+                Club.objects.annotate(members_num=Count("members")).order_by('-members_num')[0:MAX_ORDER],
+                city
             )
             update_club_at_billboard(
                 'females',
@@ -66,33 +68,36 @@ def update_billboard():
                         Case(When(members__gender='f', then=Value(1)),
                              default=Value(0), output_field=IntegerField())
                     )
-                ).order_by('-females')[0:MAX_ORDER]
+                ).order_by('-females')[0:MAX_ORDER],
+                city
             )
-            # # 总价排名
-            # filter_type = "total"
-            # best_total_values = Club.objects.filter(identified=True).order_by("-value_total")[0:MAX_ORDER]
-            # for order, club in enumerate(best_total_values, 1):
-            #     try:
-            #         old_billboard = ClubBillboard.objects.get(
-            #             club=club, version=next_billboard_version-1, scope=city, filter_type=filter_type
-            #         )
-            #         old_order = old_billboard.order
-            #         ClubBillboard.objects.create(
-            #             club=club, order=order, d_order=order - old_order, scope=city, filter_type=filter_type,
-            #             new_to_list=False
-            #         )
-            #     except ObjectDoesNotExist:
-            #         ClubBillboard.objects.create(
-            #             club=club, order=order, d_order=0, scope=city, filter_type=filter_type, new_to_list=True
-            #         )
-            # # 均价排名
-            # filter_type = "average"
-            # best_average_values = Club.objects.filter(identified=True).order_by('-value_average')[0:MAX_ORDER]
-            # for order, club in enumerate(best_average_values, 1):
-            #     try:
-            #         old_billboard = ClubBillboard.objects.get(
-            #             ClubBillboard.objects
-            #         )
+    city = u'全国'
+    update_club_at_billboard(
+        "total",
+        Club.objects.filter(identified=True).order_by("-value_total")[0:MAX_ORDER],
+        city
+    )
+    update_club_at_billboard(
+        'average',
+        Club.objects.filter(identified=True).order_by('-value_average')[0:MAX_ORDER],
+        city
+    )
+    update_club_at_billboard(
+        'members',
+        Club.objects.annotate(members_num=Count("members")).order_by('-members_num')[0:MAX_ORDER],
+        city
+    )
+    update_club_at_billboard(
+        'females',
+        Club.objects.annotate(
+            females=Sum(
+                Case(When(members__gender='f', then=Value(1)),
+                     default=Value(0), output_field=IntegerField())
+            )
+        ).order_by('-females')[0:MAX_ORDER],
+        city
+    )
+
 
 
 @app.task()
