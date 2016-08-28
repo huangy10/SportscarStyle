95e1bef89547cce10a85692b3e8c7eac2ee98ac6
Woody Huang <woody1573@163.com>
2016-08-28 15:18:06 +0800

    support for club authentication management in admin site

--- Club/admin.py
+++ Club/admin.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from django.contrib import admin
 from django.db.models import Count
-
+from django.utils import timezone
 from Club.models import Club, ClubAuthRequest
 # Register your models here.
 
@@ -23,3 +23,32 @@ class ClubAdmin(admin.ModelAdmin):
         return obj.members_num
     members_num.short_description = u"成员数量"
     members_num.admin_order_field = "members_count"
+
+
+@admin.register(ClubAuthRequest)
+class ClubAuthRequestAdmin(admin.ModelAdmin):
+
+    fields = ("approve", "checked", "city", "description", "club_link")
+    readonly_fields = ("city", "description", "club_link")
+    list_display = ("club", "description", "city", "approve", "checked")
+    list_filter = ('checked', "approve")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def save_model(self, request, obj, form, change):
+        old_obj = ClubAuthRequest.objects.get(id=obj.id)
+        if not old_obj.checked and obj.checked:
+            club = obj.club
+            club.identified = True
+            club.identified_at = timezone.now()
+            club.save()
+        super(ClubAuthRequestAdmin, self).save_model(request, obj, form, change)
+
+    def club_link(self, obj):
+        return u'<a href="/admin/Club/club/%s">%s</a>' % (obj.club.id, obj.club.name)
+    club_link.short_description = u'申请俱乐部'
+    club_link.allow_tags = True
